da488b78d994ba9eac58a0c7b93de9c1edb330b0
David Jackson <david.jackson@sykes.com>
2014-06-28 16:41:43 -0500

    'create server' page (no save yet)

--- client/app/routes/servers/new.js
+++ client/app/routes/servers/new.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.get('store').createRecord('server', {
+      name: 'New Server'
+    });
+  },
+
+  deactivate: function() {
+    var model = this.get('controller.model');
+    model.rollback();
+    if (model.get('isNew')) {
+      model.deleteRecord();
+    }
+  }
 });
