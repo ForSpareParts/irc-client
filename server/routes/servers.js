c71aead76c228d01287943f357a9ee9963073c29
David Jackson <david.jackson@sykes.com>
2014-07-03 17:20:48 -0500

    Made REST router generic.

--- server/routes/servers.js
+++ server/routes/servers.js
@@ -3,69 +3,65 @@ var router = express.Router();
 
 var Server = require('../models').Server;
 
-var getAll = function(req, res) {
-  var promise = Server.all();
+var modelRestRouter = function(model) {
+  var getAll = function(req, res) {
+    var promise = model.all();
 
-  promise.then(function (servers) {
-    res.send(servers);
-  })
-};
+    promise.then(function (servers) {
+      res.send(servers);
+    })
+  };
 
-var getId = function(req, res) {
-  var promise = Server.find(req.params.id);
+  var getId = function(req, res) {
+    var promise = model.find(req.params.id);
 
-  promise.then(function(server) {
-    res.send(server);
-  })
-};
+    promise.then(function(server) {
+      res.send(server);
+    })
+  };
 
-var create = function(req, res) {
-  var promise = Server.create({
-    name: req.body.name,
-    host: req.body.host,
-    port: req.body.port
-  });
+  var create = function(req, res) {
+    var promise = model.create(req.body);
 
-  promise.then(function(created) {
-    res.send(created);
-  });
-};
-
-var update = function(req, res) {
-  var promise = Server.find(req.params.id);
-
-  promise.then(function(server) {
-
-    return server.updateAttributes({
-      name: req.body.name,
-      host: req.body.host,
-      port: req.body.port
+    promise.then(function(created) {
+      res.send(created);
     });
+  };
 
-  }).then(function(server) {
+  var update = function(req, res) {
+    var promise = model.find(req.params.id);
 
-    res.send(server);
+    promise.then(function(server) {
 
-  });
+      return server.updateAttributes(req.body);
+
+    }).then(function(server) {
+
+      res.send(server);
+
+    });
+  };
+
+  var destroy = function(req, res) {
+    var promise = model.find(req.params.id);
+
+    promise.then(function(server) {
+      return server.destroy();
+    }).then(function() {
+      res.send(
+        {success: true}
+      );
+    });
+  };
+
+  router.get('/', getAll);
+  router.post('/', create);
+
+  router.get('/:id', getId);
+  router.put('/:id', update);
+  router.delete('/:id', destroy);
+
+  return router;
 };
 
-var destroy = function(req, res) {
-  var promise = Server.find(req.params.id);
-
-  promise.then(function(server) {
-    return server.destroy();
-  }).then(function() {
-    res.send(
-      {success: true}
-    );
-  });
-};
-
-router.get('/', getAll);
-router.post('/', create);
-
-router.get('/:id', getId);
-router.put('/:id', update);
-router.delete('/:id', destroy);
-
-module.exports = router;
+module.exports = modelRestRouter(Server);
