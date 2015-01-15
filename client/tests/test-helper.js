9b762dfdcebd9d275cb78fabde9804ded717efd5
David Jackson <david.jackson@sykes.com>
2015-01-14 20:28:09 -0600

    Dropping PhantomJS support from tests due to lack of PATCH support: https://github.com/ariya/phantomjs/issues/11384
    Lack of bind() was already pretty bad, but silently failing on a perfectly
    normal type of HTTP request is more than I want to work around. Supposedly,
    version 2.0 will fix this issue (see the GitHub thread) and maybe bind() as
    well, so I'll give Phantom another shot then. Until then I'll just stick with
    Chrome/FF, and look into Sauce if I want continuous integration.

--- client/tests/test-helper.js
+++ client/tests/test-helper.js
@@ -3,33 +3,6 @@ import Ember from 'ember';
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
 
-/**
- * the MDN polyfill for Function.prototype.bind, which is not supported in
- * PhantomJS 1.9.7
- */
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function(oThis) {
-    if (typeof this !== 'function') {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-    }
-
-    var aArgs   = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        FNOP    = function() {},
-        fBound  = function() {
-          return fToBind.apply(this instanceof FNOP && oThis ? this : oThis,
-                 aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    FNOP.prototype = this.prototype;
-    fBound.prototype = new FNOP();
-
-    return fBound;
-  };
-}
-
 setResolver(resolver);
 
 document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
