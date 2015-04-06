917cda5b0e29ac4f4facf3e426ad35deef01776c
David Jackson <altmail@jackson-wallace.net>
2015-04-05 22:07:14 -0500

    Better tests for Message model/adapter.

--- client/tests/unit/adapters/message-test.js
+++ client/tests/unit/adapters/message-test.js
@@ -1,8 +1,24 @@
 /* jshint expr:true */
+import Ember from 'ember';
 import {
   describeModule,
   it
 } from 'ember-mocha';
+/* global sinon */
+
+var stub;
+
+var MESSAGE_JSON = {
+  message: {
+    id: 100,
+
+    nick: "testSocketNick",
+    channel_id: 1,
+    time: new Date('2000-01-01T00:00:00').toISOString(),
+
+    contents: 'This message is fake!'
+  }
+};
 
 describeModule(
   'adapter:message',
@@ -12,10 +28,26 @@ describeModule(
     // needs: ['serializer:foo']
   },
   function() {
+
+    beforeEach(function() {
+      stub = sinon.stub().returns(Ember.RSVP.resolve(null));
+
+      this.container.register('store:main', Ember.Object.extend({
+        pushPayload: stub
+      }));
+    });
+
     // Replace this with your real tests.
-    it('exists', function() {
+    it('should receive messages from the server', function() {
       var adapter = this.subject();
-      expect(adapter).to.be.ok;
+
+      andThen(function() {
+        adapter.socket.localEmit('message', MESSAGE_JSON);
+      });
+
+      andThen(function() {
+        stub.calledWith(MESSAGE_JSON);
+      });
     });
   }
 );
--- client/tests/unit/models/message-test.js
+++ client/tests/unit/models/message-test.js
@@ -3,6 +3,9 @@ import {
   describeModel,
   it
 } from 'ember-mocha';
+/* global moment */
+
+var UTC_STRING = "2015-01-10T12:05:00.000Z";
 
 describeModel(
   'message',
@@ -10,7 +13,6 @@ describeModel(
   {
     // Specify the other units that are required for this test.
     needs: [
-      'adapter:message',
       'model:connection',
       'model:server',
       'model:channel',
@@ -19,34 +21,28 @@ describeModel(
     ]
   },
   function() {
-    it('should receive messages from the server', function() {
-      var model = this.subject();
-      var store = this.store();
-
-      var adapter = this.container.lookup('adapter:message');
-
-      andThen(function() {
-        adapter.socket.localEmit('message', {
-          message: {
-            id: 100,
-
-            nick: "testSocketNick",
-            channel_id: 1,
-            time: new Date('2000-01-01T00:00:00').toISOString(),
-
-            contents: 'This message is fake!'
-          }
-        });
+    it('should expose the formatted time as a property', function() {
+      var message = this.subject({
+        time: moment(UTC_STRING).utc()
       });
 
-      andThen(function() {
-        var message = store.getById('message', 100);
+      equal(message.get('shortTime'), '12:05 PM');
+    });
 
-        equal(message.get('nick'), 'testSocketNick');
-        equal(message.get('contents'), 'This message is fake!');
+    it('should expose a message\'s type via properties', function() {
+      var message = this.subject({
+        type: 'join'
       });
 
+      assert.isTrue(message.get('isJoin'));
+      assert.isTrue(message.get('isEvent'));
 
+      message.set('type', 'part');      
+      assert.isTrue(message.get('isPart'));
+      assert.isTrue(message.get('isEvent'));
+
+      message.set('type', '');
+      assert.isFalse(message.get('isEvent'));
     });
 
   }
