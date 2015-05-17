8cfd1fee4f3de13ac8bdad7440e67b461ccad545
David Jackson <altmail@jackson-wallace.net>
2015-05-16 22:59:31 -0500

    Changed messaging sending to use websockets, removed some old code.

--- client/app/routes/channel.js
+++ client/app/routes/channel.js
@@ -2,50 +2,17 @@ import Ember from 'ember';
 /* global moment */
 
 export default Ember.Route.extend({
+  socket: Ember.inject.service('socket'),
+
   model: function(params) {
     var store = this.get('store');
     return store.find('channel', params.channel_id);
   },
 
   actions: {
-
     /** Send messageText to the Channel. */
     sendMessage: function(messageText, callback) {
-      //NOTE: see message-input.js for reasoning behind callback
-      var channel = this.modelFor(this.routeName);
-      var store = channel.get('store');
-
-      var message;
-
-      channel.get('server')
-
-      .then(function(server) {
-        message = store.createRecord('message', {
-          nick: server.get('nick'),
-          channel: channel,
-          time: moment(new Date(Date.now())),
-
-          contents: messageText,
-        });
-
-        return message.save();
-      })
-
-      .then(function(savedMessage) {
-        channel.get('messages').addObject(savedMessage);
-
-        if (callback){
-          callback();
-        }
-
-      })
-
-      .catch(function(reason) {
-        message.deleteRecord();
-        if (callback){
-          callback(reason);
-        }
-      });
+      this.get('socket').emit('message', this.get('model.id'), messageText);
     }
   }
 });
--- client/app/routes/channels/new.js
+++ client/app/routes/channels/new.js
@@ -7,19 +7,6 @@ export default Ember.Route.extend({
     return Ember.Object.create();
   },
 
-  // deactivate: function() {
-  //   var model = this.get('controller.model');
-  //   if (model.get('dirtyType') === 'created') {
-  //     model.deleteRecord();
-
-
-  //     //if we were rejoining a channel that existed before, there'll be an extra
-  //     //copy of the channel in the 
-  //     this.send('refreshJoined');
-  //   }
-
-  // },
-
   actions: {
     save: function(model) {
       var self = this;
